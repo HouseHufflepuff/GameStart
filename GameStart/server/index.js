858a018fd633a72a17d7de8fa8e361dcade38f33
Shawn <sro1115@outlook.com>
2022-09-30 17:35:33 -0700

    basic set up w/ schema design

---
+++ GameStart/server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/router.js');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const corsOptions ={
+  origin:'*',
+  credentials:true,
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/api', router);
+
+
+router.use()
+
+app.listen(8000, () => {
+  console.log('server started on port 8000')
+});
