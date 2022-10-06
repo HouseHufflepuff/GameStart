08979ba8cd7893dcfad3a5f187a28a9796480f1d
danielczweig <danielczweig@gmail.com>
2022-10-06 13:01:54 -0700

    added more game info to game details feature, added some more css

--- GameStart/App.js
+++ GameStart/App.js
@@ -3,18 +3,22 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 // import { NavigationContainer } from '@react-navigation/native';
 // import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import Main from './components/Main.jsx';
-import CreateAccount from './components/register/CreateAccount.js';
-import RegisterConsoles from './components/register/RegisterConsoles.js';
-import ProfilePicture from './components/register/ProfilePicture.js';
-import RegisterGames from './components/register/RegisterGames.js';
+// import CreateAccount from './components/register/CreateAccount.js';
+// import RegisterConsoles from './components/register/RegisterConsoles.js';
+// import ProfilePicture from './components/register/ProfilePicture.js';
+// import RegisterGames from './components/register/RegisterGames.js';
+import GameDetails from './GameDetails.js';
+import Search from './Search.js'
 
 function App() {
+  const[gameId, setGameId] = useState('325094')
 
   // const Stack = createNativeStackNavigator();
 
   return (
-    <RegisterGames/>
-
+    // <RegisterGames/>
+    // <Search />
+    <GameDetails gameId={gameId}/>
   //   <NavigationContainer>
   //     <Stack.Navigator screenOptions={{ headerShown: false }}>
   //       <Stack.Screen options={{ headerShown: false }} name="Login" component={Main} />
