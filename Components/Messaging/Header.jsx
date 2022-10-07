6e679675ae75f86c0cf9f9fcd714ac9e65728d50
gene <g_lo_27@hotmail.com>
2022-10-06 19:10:17 -0700

    added source

--- Components/Messaging/Header.jsx
+++ Components/Messaging/Header.jsx
@@ -10,7 +10,7 @@ export default function Header({otherUser, incoming, outgoing}) {
         <Text style={{ position: 'absolute', color:'white', padding:'3%', fontSize:40}}>&#x2039;</Text>
       </View>
       <View style={{alignSelf:'center', justifyContent:'center', marginTop:'7%'}}>
-        <Image style={{backgroundColor:'white', width: 35, height: 35 ,borderRadius:50}}>
+        <Image style={{backgroundColor:'white', width: 35, height: 35 ,borderRadius:50} source={'https://media.rawg.io/media/games/9f3/9f3c513b301d8d7250a64dd7e73c62df.jpg'}}>
         </Image>
         <Text style={styles.headerText}>{otherUser}</Text>
       </View>
