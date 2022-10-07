3ee68871d86900220ffd9b4c972b1c1faee17622
danielczweig <danielczweig@gmail.com>
2022-10-07 01:24:05 -0700

    made css updates to search bar (75% complete)

--- Components/Search/SearchBar.js
+++ Components/Search/SearchBar.js
@@ -3,19 +3,23 @@ import { LinearGradient } from 'expo-linear-gradient';
 // import { Card, Icon } from "@rneui/themed";
 import { StatusBar } from 'expo-status-bar';
 import { FlatList, Image, Keyboard, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import axios from 'axios';
 
 const consoles = {
-  'PC': 'https://cdn.freebiesupply.com/logos/large/2x/microsoft-windows-22-logo-png-transparent.png',
-  'PlayStation': 'https://www.freepnglogos.com/uploads/playstation-png-logo/navy-playstation-png-logo-5.png',
-  'Xbox': 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/1024px-Xbox_one_logo.svg.png',
-  'Nintendo': 'https://www.freepnglogos.com/uploads/nintendo-logo-png/file-micrologo-nintendo-n-logo-circle-18.png',
+  // 'PC': 'https://cdn.freebiesupply.com/logos/large/2x/microsoft-windows-22-logo-png-transparent.png',
+  // 'PlayStation': 'https://www.freepnglogos.com/uploads/playstation-png-logo/navy-playstation-png-logo-5.png',
+  // 'Xbox': 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Xbox_one_logo.svg/1024px-Xbox_one_logo.svg.png',
+  // 'Nintendo': 'https://www.freepnglogos.com/uploads/nintendo-logo-png/file-micrologo-nintendo-n-logo-circle-18.png',
   // 'iOS': 'https://alchemyimmersive.com/wp-content/uploads/sites/4/2020/04/apple-logo-transparent.png',
   // 'Android': 'https://cdn.freebiesupply.com/logos/large/2x/android-logo-png-transparent.png',
+  // 'PlayStation': <MaterialCommunityIcons name="sony-playstation" size={18} />,
+  // 'Xbox': <MaterialCommunityIcons name="microsoft-xbox" size={15} />,
+  // 'Nintendo': <MaterialCommunityIcons name="nintendo-switch" size={15} />,
 }
 
 const consoleCodes = {
-  'PC': '4',
+  // 'PC': '4',
   'PlayStation 5': '187',
   'PlayStation 4': '18',
   'PlayStation 3': '16',
@@ -44,10 +48,19 @@ const getYear = (date) => {
   return newDate.getFullYear()
 }
 
+// const getPlatforms = (platforms) => {
+//   if(platforms) {
+//     return platforms.map((p, i) => {if(p.platform.name in consoles) {
+//       return (<Image key={i} style={styles.consoleIcon} source={{uri: consoles[p.platform.name]}}/>)
+//     }})
+//   }
+//   return ''
+// }
+
 const getPlatforms = (platforms) => {
   if(platforms) {
     return platforms.map((p, i) => {if(p.platform.name in consoles) {
-      return (<Image key={i} style={styles.consoleIcon} source={{uri: consoles[p.platform.name]}}/>)
+      return consoles[p.platform.name]
     }})
   }
   return ''
@@ -89,18 +102,19 @@ export default function SearchBar( { console, callback } ) {
         ></TextInput>
         { text.length > 0 &&
           <FlatList
+            style={{marginTop: 20, width: '100%'}}
             data={games}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) =>
             <TouchableOpacity onPress={() => callback(item.id)}>
-              <LinearGradient start={{x: 1, y: 0}} end={{x: 0, y: 0}} colors={['#CAF0F8', '#90e0EF', '#00B4D8', '#0077B6', '#03045E' ]} style={styles.item}>
+              <View style={styles.item}>
                 <Image style={styles.image} source={{uri: item.background_image}}/>
                 <View style={styles.description}>
                   <Text style={styles.title}>{item.name}</Text>
                   <Text>{`Release year: ${getYear(item.released)}`}</Text>
-                  <View style={styles.consoles}>{getPlatforms(item.parent_platforms)}</View>
+                  {/* <View style={styles.consoles}>{getPlatforms(item.parent_platforms)}</View> */}
                 </View>
-              </LinearGradient>
+              </View>
             </TouchableOpacity>
             }
           />
@@ -114,6 +128,7 @@ export default function SearchBar( { console, callback } ) {
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
+    // flex: 1,
   },
   search: {
     height: 60,
@@ -134,27 +149,37 @@ const styles = StyleSheet.create({
   },
   item: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    borderColor: 'black',
+    // flexWrap: 'wrap',
+    left: 10,
+    right: 10,
+    width: '90%',
     borderWidth: 1,
-    borderRadius: 10,
-    padding: 10,
-    marginVertical: 2,
+    borderRadius: 5,
+    borderColor: 'white',
+    backgroundColor: '#121212',
+    marginVertical: 10,
     marginHorizontal: 10,
   },
   image: {
     height: 100,
     width: 100,
-    borderRadius: 10,
-    marginRight: 10
+    borderRadius: 5,
+    marginRight: 15
   },
   description: {
-    width: '65%',
+    padding: 5,
+    width: '60%',
+    // borderColor: 'red',
+    // borderWidth: 1,
   },
   title: {
+    // borderColor: 'red',
+    // borderWidth: 1,
+    flexWrap: 'wrap',
     fontSize: 20,
     fontWeight: 'bold',
     numberOfLines: 2,
+    color: 'white',
   },
   consoles: {
     flex: 1,
