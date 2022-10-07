1dee101e9190130129224e56d6c4931937246790
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-10-06 23:09:52 -0700

    resolved

--- server/routes/UsersRouter.js
+++ server/routes/UsersRouter.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-const Router = require('express').Router();
-const { insertUser, getUsers } = require('../controllers/users.js');
-
-Router.use('/register', insertUser);
-Router.get('/register', getUsers);
-=======
 const usersRouter = require('express').Router();
 
-const {insertAddress, insertUser, insertConsoles, changeProfilePic, getTradeCounter} = require('../controllers/users.js');
->>>>>>> de0c967312bc31bbd75604985007652fd347ba5f
+const { insertAddress, insertUser, insertConsoles, changeProfilePic, getTradeCounter } = require('../controllers/users.js');
 
 usersRouter.post('/register', insertUser);
 
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,26 +1,20 @@
 const Router = require('express').Router();
 const usersRouter = require('./UsersRouter.js');
 const gamesRouter = require('./GamesRouter.js');
-<<<<<<< HEAD
 const LocationsRouter = require('./LocationsRouter.js');
 // const TradesRouter = require('./TradesRouter.js');
 // const MessagesRouter = require('./MessagesRouter.js');
 // const {insertUser} = require('../controllers/users.js');
-=======
 const tradesRouter = require('./TradesRouter.js');
 const messagesRouter = require('./MessagesRouter.js');
->>>>>>> de0c967312bc31bbd75604985007652fd347ba5f
 
 Router.use('/users', usersRouter);
 Router.use('/games', gamesRouter);
-<<<<<<< HEAD
 Router.use('/locations', LocationsRouter);
 // Router.use('/trades', TradesRouter);
 // Router.use('/messages', MessagesRouter);
-=======
 Router.use('/trades', tradesRouter);
 Router.use('/messages', messagesRouter);
->>>>>>> de0c967312bc31bbd75604985007652fd347ba5f
 
 
 module.exports = Router;
